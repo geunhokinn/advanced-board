2f8dbce1f43226a0387e11b52602caf4e5d7f937
geunhokinn <gghh1220@gmail.com>
2024-08-12 22:46:55 +0900

    feat: 프로젝트 생성

---
+++ src/test/java/com/example/advancedboard/AdvancedBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.advancedboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvancedBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
