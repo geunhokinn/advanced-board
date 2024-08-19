2db9b44bf44e640cf4fc6d11c0055a012c00b65a
geunhokinn <gghh1220@gmail.com>
2024-08-19 23:05:47 +0900

    feat: Comment 엔티티 작성 - 연관관계 설정 및 빌더 메서드 생성

--- src/main/java/com/example/advancedboard/domain/Comment.java
+++ src/main/java/com/example/advancedboard/domain/Comment.java
@@ -1,14 +1,55 @@
 package com.example.advancedboard.domain;
 
-import java.time.LocalDateTime;
+import jakarta.persistence.*;
+import lombok.*;
 
-public class Comment {
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(indexes = {
+        @Index(columnList = "content"),
+        @Index(columnList = "createdAt"),
+        @Index(columnList = "CreatedBy")
 
+})
+public class Comment extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Setter
+    @ManyToOne(optional = false)
     private Article article; // 게시글 (id)
+
+    @Setter
+    @Column(nullable = false, length = 500)
     private String content; // 내용
-    private LocalDateTime createdAt; // 생성일시
-    private String createdBy; // 생성자
-    private LocalDateTime modifiedAt; // 수정일시
-    private String modifiedBy; // 수정자
+
+    @Builder
+    private Comment(Article article, String content) {
+        this.assignArticle(article);
+        this.content = content;
+    }
+
+    public static Comment buildComment(Article article, String content) {
+        return Comment.builder()
+                .article(article)
+                .content(content)
+                .build();
+    }
+
+    // Comment N <-> 1 Article
+    // 양방향 연관관계 편의 메서드
+    public void assignArticle(Article article) {
+        if (this.getArticle() != null) {
+            this.article.getComments().remove(this);
+        }
+
+        this.article = article;
+
+        if (!article.getComments().contains(this)) {
+            article.addComment(this);
+        }
+    }
 }
