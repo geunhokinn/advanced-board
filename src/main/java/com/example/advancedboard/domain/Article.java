bf14444f3dd0d85dbe14c5672bc229d5517b4d4b
geunhokinn <gghh1220@gmail.com>
2024-08-19 23:05:32 +0900

    feat: Article 엔티티 작성 - 연관관계 설정 및 빌더 메서드 생성

--- src/main/java/com/example/advancedboard/domain/Article.java
+++ src/main/java/com/example/advancedboard/domain/Article.java
@@ -1,15 +1,63 @@
 package com.example.advancedboard.domain;
 
-import java.time.LocalDateTime;
+import jakarta.persistence.*;
+import lombok.*;
 
-public class Article {
+import java.util.ArrayList;
+import java.util.List;
 
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(indexes = {
+        @Index(columnList = "title"),
+        @Index(columnList = "hashtag"),
+        @Index(columnList = "createdAt"),
+        @Index(columnList = "CreatedBy")
+
+})
+public class Article extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Setter
+    @Column(nullable = false)
     private String title; // 제목
+
+    @Setter
+    @Column(nullable = false, length = 10000)
     private String content; // 내용
+
+    @Setter
     private String hashtag; // 해시태그
-    private LocalDateTime createdAt; // 생성일시
-    private String createdBy; // 생성자
-    private LocalDateTime modifiedAt; // 수정일시
-    private String modifiedBy; // 수정자
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "article", cascade = CascadeType.REMOVE)
+    private final List<Comment> comments = new ArrayList<>();
+
+    @Builder
+    private Article(String title, String content, String hashtag) {
+        this.title = title;
+        this.content = content;
+        this.hashtag = hashtag;
+    }
+
+    public static Article buildArticle(String title, String content, String hashtag) {
+        return Article.builder()
+                .title(title)
+                .content(content)
+                .hashtag(hashtag)
+                .build();
+    }
+
+    // Article 1 <-> N Comment
+    // 양방향 연관관계 편의 메서드
+    public void addComment(Comment comment) {
+        this.comments.add(comment);
+
+        if (comment.getArticle() != this) {
+            comment.assignArticle(this);
+        }
+    }
 }
